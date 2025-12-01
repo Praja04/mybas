c528cd694b66392a4db589242ef7ede5886509fc
Arina <aqmarinasha@gmail.com>
2025-12-01 09:05:48 +0700

    chore(halo-security): apply auto-formatting

--- resources/views/mail/halo-security/add-ba-sop-karyawan.blade.php
+++ resources/views/mail/halo-security/add-ba-sop-karyawan.blade.php
@@ -16,18 +16,24 @@
 </head>
 
 <body>
-    <p style="text-align: center;"><img style="width: 500px; height: 150px;"
-            src="https://www.forumhrdindonesia.com/wp-content/uploads/2021/02/Perkasa-Alam-Segar-PAS.png" alt="Kop Surat">
+    <p style="text-align: center;">
+        <img style="width: 200px" src="{{ asset('assets/media/logos/logo_bas_compress.png') }}" alt="Kop Surat">
     </p>
-    <h2 style="text-align: center; color: #6A6A6A;"><span style="color: red; font-weight: bold;">NEW</span> - (Halo
-        Security) Berita Acara S.O.P Karyawan</h2>
+
+    <h2 style="text-align: center; color: #6A6A6A;">
+        <span style="color: red; font-weight: bold;">NEW</span> - (Halo
+        Security) Berita Acara S.O.P Karyawan
+    </h2>
+
     <table style="width:100%">
+
         <thead>
             <tr>
                 <th scope="col" style="font-weight: bold; color: white; background-color: #AF2120;">Informasi</th>
                 <th scope="col" style="font-weight: bold; color: white; background-color: #AF2120;">Link</th>
             </tr>
         </thead>
+
         <tbody>
             <tr>
                 <td style="font-weight: bold; text-align: center; color: #6B6A6B;">Untuk melihat data baru berita acara
@@ -44,6 +50,7 @@
                         style="text-decoration: none; color:red;">Download PDF</a></td>
             </tr>
         </tbody>
+
     </table>
 </body>
 
