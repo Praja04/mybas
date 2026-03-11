58b54dddde030f0f442cf40498af13bd45330f3c
Praja04 <rajadamang13@gmail.com>
2026-03-11 10:53:58 +0700

    FIX: PEMBAGIAN KARYWANA MENU SCAN

--- app/Http/Controllers/PembagianController.php
+++ app/Http/Controllers/PembagianController.php
@@ -29,87 +29,66 @@ class PembagianController extends Controller
 
     public function checkoutScan(Request $request)
     {
-        $data = [];
-
         $id_card = (int)$request->id_card;
 
-        // Get data dari secure access berdasarkan nomor kartu
-
+        // Ambil data karyawan dari server
         $user = DB::connection('192.168.178.44-admin')
             ->table('MSIDCARD')
-            ->select('NIK', 'EMPNM', 'CREATEDON')
-            ->where(['CARDNODEVICE' => $id_card])
-            ->orderByRaw('CAST(SUBSTR(NIK, 8) AS SIGNED) desc')
+            ->select('NIK', 'EMPNM', 'DEPTID')
+            ->where('CARDNODEVICE', $id_card)
+            ->orderByRaw('CAST(NIK AS SIGNED) desc')
             ->first();
 
-        if ($user == null) {
-            return response()->json(['success' => 0, 'message' => 'Data not found']);
+        if (!$user) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Data tidak ditemukan'
+            ]);
         }
 
         $nik = $user->NIK;
 
-        // Get foto nya
-        $_user = DB::connection('192.168.178.44-admin')
-            ->table('MSIDCARD')
-            ->select('NIK', 'BARCODE', 'CARDNODEVICE', 'EMPNM', 'DEPTID', 'FOTOBLOB')
-            ->where([
-                'BARCODE' => $nik,
-                // 'CARDNODEVICE' => $id_card
-            ])
-            ->orderByRaw('SUBSTR(NIK, 8) desc')
+        // Cek apakah sudah scan
+        $cek = PembagianKaryawan::where('id_pembagian', $request->id_pembagian)
+            ->where('nik', $nik)
             ->first();
 
-        $userArray = [];
-
-        $userArray['nik']       = $_user->NIK;
-        $userArray['nama']         = $_user->EMPNM;
-        $userArray['dept']         = $_user->DEPTID;
-        $userArray['foto']         = base64_encode($_user->FOTOBLOB);
-
-        // Cek dulu apakah hari ini ada jadwal pembagian
-        // $pembagian = Pembagian::where('tanggal_pembagian', date('Y-m-d'))->first();
-
-        // if($pembagian == null) {
-        //     return response()->json(['success' => 0, 'message' => 'Tidak ada jadwal pembagian hari ini']);
-        // }
-
-        // Cek apakah nik ini boleh ngambil
-
-        $pembagian_karyawan = PembagianKaryawan::where('id_pembagian', $request->id_pembagian)->where('nik', $nik)->first();
-        if ($pembagian_karyawan == null) {
+        if ($cek) {
             return response()->json([
                 'success' => 0,
-                'message' => 'Maaf kamu ga bisa ambil, mohon hubungi tim HRD',
-                'data' => $userArray
+                'message' => $user->EMPNM . ' sudah pernah scan pada ' . $cek->waktu_ambil,
+                'data' => [
+                    'nik' => $user->NIK,
+                    'nama' => $user->EMPNM,
+                    'dept' => $user->DEPTID
+                ]
             ]);
         }
 
-        // Cek kalau sudah pernah ngambil
-        if ($pembagian_karyawan->status_ambil != 'sudah') {
-            return response()->json([
-                'success' => 0,
-                'message' => 'Kamu belum secan, silahkan scan dulu',
-                'data' => $userArray
-            ]);
-        }
+        // Simpan data scan
+        $scan = new PembagianKaryawan;
+        $scan->id_pembagian = $request->id_pembagian;
+        $scan->nik = $user->NIK;
+        $scan->nama = $user->EMPNM;
+        $scan->department = $user->DEPTID;
 
-        $userArray['keterangan']         = $pembagian_karyawan->keterangan;
+        $scan->lokasi_pembagian = 'GA';   // default lokasi
+        $scan->pic = $request->pic;       // dari blade
 
-        // Proses pengambilan
+        $scan->status_ambil = 'sudah';
+        $scan->waktu_ambil = now();
 
-        $pic = $request->pic;
-
-        $pembagian = PembagianKaryawan::where('id_pembagian', $request->id_pembagian)->where('nik', $nik)->first();
-        $pembagian->checkout_time = date('Y-m-d H:i:s');
-        $pembagian->save();
-
+        $scan->save();
         return response()->json([
             'success' => 1,
-            'message' => 'Confirm pengambilan succeed',
-            'data' => $userArray
+            'message' => 'Scan berhasil',
+            'data' => [
+                'nik' => $user->NIK,
+                'nama' => $user->EMPNM,
+                'dept' => $user->DEPTID
+            ]
         ]);
     }
-
     public function display($lokasi = '')
     {
         $pembagian = Pembagian::orderBy('created_at', 'desc')->get();
@@ -122,98 +101,54 @@ class PembagianController extends Controller
 
     public function displayScan(Request $request)
     {
-        $data = [];
-
         $id_card = (int)$request->id_card;
 
-        // Get data dari secure access berdasarkan nomor kartu
+        // Ambil data karyawan dari server
         $user = DB::connection('192.168.178.44-admin')
             ->table('MSIDCARD')
-            ->select('NIK', 'EMPNM', 'CREATEDON')
-            ->where(['CARDNODEVICE' => $id_card])
+            ->select('NIK', 'EMPNM', 'DEPTID')
+            ->where('CARDNODEVICE', $id_card)
             ->orderByRaw('CAST(NIK AS SIGNED) desc')
             ->first();
 
-        if ($user == null) {
-            return response()->json(['success' => 0, 'message' => 'Data not found']);
+        if (!$user) {
+            return response()->json([
+                'success' => 0,
+                'message' => 'Data tidak ditemukan'
+            ]);
         }
 
         $nik = $user->NIK;
 
-        // Get foto nya
-        $_user = DB::connection('192.168.178.44-admin')
-            ->table('MSIDCARD')
-            ->select('NIK', 'BARCODE', 'CARDNODEVICE', 'EMPNM', 'DEPTID', 'FOTOBLOB')
-            ->where([
-                'BARCODE' => $nik,
-                // 'CARDNODEVICE' => $id_card
-            ])
-            ->orderByRaw('CAST(NIK AS SIGNED) desc')
+        // Cek apakah sudah scan
+        $cek = PembagianKaryawan::where('id_pembagian', $request->id_pembagian)
+            ->where('nik', $nik)
             ->first();
 
-        $userArray = [];
-
-        $userArray['nik']       = $_user->NIK;
-        $userArray['nama']         = $_user->EMPNM;
-        $userArray['dept']         = $_user->DEPTID;
-        $userArray['foto']         = base64_encode($_user->FOTOBLOB);
-
-        // Cek dulu apakah hari ini ada jadwal pembagian
-        // $pembagian = Pembagian::where('tanggal_pembagian', date('Y-m-d'))->first();
-
-        // if($pembagian == null) {
-        //     return response()->json(['success' => 0, 'message' => 'Tidak ada jadwal pembagian hari ini']);
-        // }
-
-        // Cek apakah nik ini boleh ngambil
-
-        $pembagian_karyawan = PembagianKaryawan::where('id_pembagian', $request->id_pembagian)->where('nik', $nik)->where('lokasi_pembagian', $request->lokasi)->first();
-        if ($pembagian_karyawan == null) {
-
-            // Cek apakah ada pembagian di tempat lain
-            $pembagian_tempat_lain = PembagianKaryawan::where('id_pembagian', $request->id_pembagian)->where('nik', $nik)->orderBy('created_at', 'desc')->first();
-
-            if ($pembagian_tempat_lain != null) {
-                return response()->json([
-                    'success' => 0,
-                    'message' => 'Tidak boleh ambil di sini, tapi di : ' . $pembagian_tempat_lain->lokasi_pembagian,
-                    'data' => $userArray
-                ]);
-            }
-
+        if ($cek) {
             return response()->json([
                 'success' => 0,
-                'message' => 'Maaf kamu ga bisa ambil, mohon hubungi tim HRD',
-                'data' => $userArray
+                'message' => $user->EMPNM . ' sudah pernah scan pada ' . $cek->waktu_ambil
             ]);
         }
 
-        // Cek kalau sudah pernah ngambil
-        if ($pembagian_karyawan->status_ambil == 'sudah') {
-            return response()->json([
-                'success' => 0,
-                'message' => 'Kamu udah pernah ambil - ' . $pembagian_karyawan->waktu_ambil,
-                'data' => $userArray
-            ]);
-        }
-
-        $userArray['department']         = $pembagian_karyawan->department;
-        $userArray['keterangan']         = $pembagian_karyawan->keterangan;
-
-        // Proses pengambilan
-
-        $pic = $request->pic;
-
-        $pembagian = PembagianKaryawan::where('id_pembagian', $request->id_pembagian)->where('nik', $nik)->first();
-        $pembagian->pic = $pic;
-        $pembagian->status_ambil = 'sudah';
-        $pembagian->waktu_ambil = date('Y-m-d H:i:s');
-        $pembagian->save();
+        // Simpan data scan
+        $scan = new PembagianKaryawan;
+        $scan->id_pembagian = $request->id_pembagian;
+        $scan->nik = $user->NIK;
+        $scan->department = $user->DEPTID;
+        $scan->status_ambil = 'sudah';
+        $scan->waktu_ambil = now();
+        $scan->save();
 
         return response()->json([
             'success' => 1,
-            'message' => 'Confirm pengambilan succeed',
-            'data' => $userArray
+            'message' => 'Scan berhasil',
+            'data' => [
+                'nik' => $user->NIK,
+                'nama' => $user->EMPNM,
+                'dept' => $user->DEPTID
+            ]
         ]);
     }
 
