0db1abc1774ef75ec1ea28dda742c791821080ec
Ananda Rizky <anndxrzky02@gmail.com>
2026-06-22 08:32:41 +0700

    feat: Membuat skenario untuk riwayat parkir menggunakan Dummy Data

--- app/Http/Controllers/BarrierGate/ParkingTapController.php
+++ app/Http/Controllers/BarrierGate/ParkingTapController.php
@@ -35,6 +35,83 @@ class ParkingTapController extends Controller
         $data              = $request->validated();
         $data['tapped_at'] = now();
 
+        $cardOwner = ParkingHistories::where('sn_card', $data['sn_card'])
+            ->where('nik', $data['nik'])
+            ->where('nama', '!=', $data['nama'])
+            ->latest()
+            ->first();
+
+        if (! empty($cardOwner)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Ini bukan kartu Anda!',
+                'data'    => [
+                    'nama' => $data['nama'],
+                ],
+            ], 404);
+        }
+
+        $notOwner = ParkingHistories::where('sn_card', $data['sn_card'])
+            ->where('nik', '!=', $data['nik'])
+            ->latest()
+            ->first();
+
+        if (! empty($notOwner)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Kartu sudah terikat dengan NIK lain',
+                'data'    => $notOwner->only('nik'),
+            ], 409);
+        }
+
+        // $possibleManyCards = ParkingHistories::where('nik', $data['nik'])->where('sn_card', '!=', $data['sn_card'])->exists();
+
+        // if ($possibleManyCards) {
+        //     return response()->json([
+        //         'success' => false,
+        //         'message' => 'NIK ini sudah terdaftar menggunakan kartu lain.',
+        //         'data'    => [
+        //             'sn_card' => $data['sn_card'],
+        //         ],
+        //     ], 422);
+        // }
+
+        $latestTap = ParkingHistories::where([
+            'nik'     => $data['nik'],
+            'nama'    => $data['nama'],
+            'sn_card' => $data['sn_card'],
+        ])->latest()->first();
+
+        if ($latestTap) {
+            if ($latestTap->status == $data['status']) {
+                if (strtoupper(trim($data['status'])) == 'IN') {
+                    $message = 'Kartu sedang berada di dalam area parkir';
+                } else {
+                    $message = 'Kartu sedang berada di luar area parkir';
+                }
+
+                return response()->json([
+                    'success' => false,
+                    'message' => $message,
+                    'data'    => [
+                        'sn_card' => $data['sn_card'],
+                        'status'  => $data['status'],
+                    ],
+                ], 409);
+            }
+        } else {
+            if (strtoupper(trim($data['status'])) == 'OUT') {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Kartu sedang berada di luar area parkir',
+                    'data'    => [
+                        'sn_card' => $data['sn_card'],
+                        'status'  => $data['status'],
+                    ],
+                ], 409);
+            }
+        }
+
         $histories = ParkingHistories::create($data);
 
         return response()->json([
