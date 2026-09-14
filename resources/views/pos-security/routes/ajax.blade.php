9a92679fe4c6ead58c2a46d8697613fa0a1b7fe9
Tohadi <toadiasley243@gmail.com>
2026-09-14 15:31:24 +0700

    feat: update warehouse API URL and timeout settings, enhance logging for vehicle transactions

--- resources/views/pos-security/routes/ajax.blade.php
+++ resources/views/pos-security/routes/ajax.blade.php
@@ -8,6 +8,7 @@
     var API_FORM_SEARCH_KENDARAAN_IN = '{{ route('ajax.pos-security.cek-kendaraan.search-in') }}';
     var API_FORM_SEARCH_KENDARAAN_OUT = '{{ route('ajax.pos-security.cek-kendaraan.search-out') }}';
     var API_CEK_KENDARAAN_SHOW = '{{ route('ajax.pos-security.cek-kendaraan.show') }}';
+    var API_CEK_KENDARAAN_WAREHOUSE_STATUS = '{{ route('ajax.pos-security.cek-kendaraan.warehouse-status', ':nopol') }}';
 
     var API_BLACKLIST_SHOW = '{{ route('ajax.pos-security.blacklist.show') }}';
     var API_BLACKLIST_CANCEL = '{{ route('ajax.pos-security.blacklist.cancel') }}';
